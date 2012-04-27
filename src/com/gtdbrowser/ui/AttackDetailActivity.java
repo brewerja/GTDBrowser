c81cdaf7005a059997e8c1c81f838672f581a62c
John Brewer <brewerja@gmail.com>
2012-04-26 22:12:30 -0400

    Added a rough tabs page for the attack detail.

--- src/com/gtdbrowser/ui/AttackDetailActivity.java
+++ src/com/gtdbrowser/ui/AttackDetailActivity.java
@@ -1,11 +1,14 @@
 package com.gtdbrowser.ui;
 
+import com.gtdbrowser.R;
 import com.gtdbrowser.data.provider.GtdContent.AttackDao;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 import android.widget.TextView;
 
 public class AttackDetailActivity extends Activity {
@@ -15,6 +18,7 @@ public class AttackDetailActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.attack_detail);
 
 		Intent intent = getIntent();
 		id = intent.getStringExtra("id");
@@ -23,8 +27,31 @@ public class AttackDetailActivity extends Activity {
 				null);
 		c.moveToFirst();
 		String newString = c.getString(AttackDao.CONTENT_CITY_COLUMN);
-		TextView tv = new TextView(this);
+		TextView tv = (TextView) findViewById(R.id.txt1);
 		tv.setText("GTD ID: " + id + "\n" + newString);
-		setContentView(tv);
+
+		TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+		tabHost.setup();
+
+		TabSpec spec1 = tabHost.newTabSpec("Tab 1");
+		spec1.setContent(R.id.tab1);
+		spec1.setIndicator("Tab 1");
+
+		TabSpec spec2 = tabHost.newTabSpec("Tab 2");
+		spec2.setIndicator("Tab 2");
+		spec2.setContent(R.id.tab2);
+
+		TabSpec spec3 = tabHost.newTabSpec("Tab 3");
+		spec3.setIndicator("Tab 3");
+		spec3.setContent(R.id.tab3);
+
+		TabSpec spec4 = tabHost.newTabSpec("Tab 4");
+		spec4.setIndicator("Tab 4");
+		spec4.setContent(R.id.tab4);
+
+		tabHost.addTab(spec1);
+		tabHost.addTab(spec2);
+		tabHost.addTab(spec3);
+		tabHost.addTab(spec4);
 	}
 }
